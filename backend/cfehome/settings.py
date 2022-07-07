ab8ba982da8c36be23c077d037a222302c377cb1
Kyle <wiseworks.2022@gmail.com>
2022-07-06 20:31:08 -0500

    updated views

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
     'products',
 ]
@@ -112,15 +113,27 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L1on = True 
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "api.authentication.TokenAuthentication"
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ]
+}
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,76 +1,73 @@
 from rest_framework import authentication, generics, mixins, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-#
+# from django.http import Http404
 from django.shortcuts import get_object_or_404
 
+from api.authentication import TokenAuthentication
+
 from .models import Product
+from .permissions import IsStaffEditorPermission
 from .serializers import ProductSerializer
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None
         if content is None:
-            content = title 
+            content = title
         serializer.save(content=content)
-        # Send a Django Signal
-product_list_create_view = ProductListCreateAPIView.as_view()
-####################################################
+        # send a Django signal
 
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk' ??
+
 product_detail_view = ProductDetailAPIView.as_view()
-####################################################
+
 
 class ProductUpdateAPIView(generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_field = 'pk' 
+    lookup_field = 'pk'
 
     def perform_update(self, serializer):
         instance = serializer.save()
         if not instance.content:
-            instance.content = instance.title 
+            instance.content = instance.title
             ## 
+
 product_update_view = ProductUpdateAPIView.as_view()
-####################################################
+
 
 class ProductDestroyAPIView(generics.DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_field = 'pk' 
+    lookup_field = 'pk'
 
     def perform_destroy(self, instance):
-        # instance
+        # instance 
         super().perform_destroy(instance)
 
 product_destroy_view = ProductDestroyAPIView.as_view()
-####################################################
 
-# class ProductListAPIView(generics.RetrieveAPIView):
-#     '''''
-#     Not going to use 
-#     '''''
+# class ProductListAPIView(generics.ListAPIView):
+#     '''
+#     Not gonna use this method
+#     '''
 #     queryset = Product.objects.all()
 #     serializer_class = ProductSerializer
-#     # lookup_field = 'pk'
 
-# # This is extra code
 # product_list_view = ProductListAPIView.as_view()
-# ####################################################
 
-# class CreateAPIView(mixins.CreateModelMixin, generics.GenericAPIView):
-#     pass
 
 class ProductMixinView(
     mixins.CreateModelMixin,
@@ -82,8 +79,7 @@ class ProductMixinView(
     serializer_class = ProductSerializer
     lookup_field = 'pk'
 
-    def get(self, request, *args, **kwargs):# HTTP Get -->
-        print(args, kwargs)
+    def get(self, request, *args, **kwargs): #HTTP -> get
         pk = kwargs.get("pk")
         if pk is not None:
             return self.retrieve(request, *args, **kwargs)
@@ -91,22 +87,22 @@ class ProductMixinView(
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
-
+    
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None
         if content is None:
-            content = "this is a single view doing cool stuff" 
+            content = "this is a single view doing cool stuff"
         serializer.save(content=content)
+
+    # def post(): #HTTP -> post
 
 product_mixin_view = ProductMixinView.as_view()
 
-    #def post():#HTTP post ->
-@api_view(["GET", "POST"])
+@api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
-    method = request.method
-
+    method = request.method  
 
     if method == "GET":
         if pk is not None:
@@ -115,20 +111,161 @@ def product_alt_view(request, pk=None, *args, **kwargs):
             data = ProductSerializer(obj, many=False).data
             return Response(data)
         # list view
-        pass 
-        queryset = Product.objects.all()
+        queryset = Product.objects.all() 
         data = ProductSerializer(queryset, many=True).data
         return Response(data)
 
-
     if method == "POST":
         # create an item
-        serializer = ProductSerializer(data=request.data)    
+        serializer = ProductSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-                title = serializer.validated_data.get('title')
-                content = serializer.validated_data.get('content') or None
-                if content is None:
-                    content = title 
-                serializer.save(content=content)
-                return Response(serializer.data)
-        return Response({"Invalid": "not good data"}, status=400),
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"}, status=400)
+
+
+# from rest_framework import authentication, generics, mixins, permissions
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+# #
+# from django.shortcuts import get_object_or_404
+# from api.authentication import TokenAuthentication 
+
+
+# from .permissions import IsStaffEditorPermission
+# from .models import Product
+# from .serializers import ProductSerializer
+
+# class ProductListCreateAPIView(generics.ListCreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     authentication_classes = [
+#         authentication.SessionAuthentication,
+#         authentication.TokenAuthentication
+#     ]
+#     permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
+
+#     def perform_create(self, serializer):
+#         # serializer.save(user=self.request.user)
+#         title = serializer.validated_data.get('title')
+#         content = serializer.validated_data.get('content') or None
+#         if content is None:
+#             content = title 
+#         serializer.save(content=content)
+#         # Send a Django Signal
+# product_list_create_view = ProductListCreateAPIView.as_view()
+# ########################################################################
+
+
+# class ProductDetailAPIView(generics.RetrieveAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     # lookup_field = 'pk' ??
+# product_detail_view = ProductDetailAPIView.as_view()
+# ##########################################################################
+
+# class ProductUpdateAPIView(generics.UpdateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+#     lookup_field = 'pk' 
+
+#     def perform_update(self, serializer):
+#         instance = serializer.save()
+#         if not instance.content:
+#             instance.content = instance.title 
+#             ## 
+# product_update_view = ProductUpdateAPIView.as_view()
+# #######################################################################
+
+# class ProductDestroyAPIView(generics.DestroyAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     lookup_field = 'pk' 
+
+#     def perform_destroy(self, instance):
+#         # instance
+#         super().perform_destroy(instance)
+
+# product_destroy_view = ProductDestroyAPIView.as_view()
+# ###############################################################
+
+# # class ProductListAPIView(generics.RetrieveAPIView):
+# #     '''''
+# #     Not going to use 
+# #     '''''
+# #     queryset = Product.objects.all()
+# #     serializer_class = ProductSerializer
+# #     # lookup_field = 'pk'
+
+# # # This is extra code
+# # product_list_view = ProductListAPIView.as_view()
+# # ####################################################
+
+# # class CreateAPIView(mixins.CreateModelMixin, generics.GenericAPIView):
+# #     pass
+
+# class ProductMixinView(
+#     mixins.CreateModelMixin,
+#     mixins.ListModelMixin,
+#     mixins.RetrieveModelMixin,
+#     generics.GenericAPIView
+#     ):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     lookup_field = 'pk'
+
+#     def get(self, request, *args, **kwargs):# HTTP Get -->
+#         print(args, kwargs)
+#         pk = kwargs.get("pk")
+#         if pk is not None:
+#             return self.retrieve(request, *args, **kwargs)
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+#     def perform_create(self, serializer):
+#         # serializer.save(user=self.request.user)
+#         title = serializer.validated_data.get('title')
+#         content = serializer.validated_data.get('content') or None
+#         if content is None:
+#             content = "this is a single view doing cool stuff" 
+#         serializer.save(content=content)
+
+# product_mixin_view = ProductMixinView.as_view()
+# #######################################################################
+#     #def post():#HTTP post ->
+# @api_view(["GET", "POST"])
+# def product_alt_view(request, pk=None, *args, **kwargs):
+#     method = request.method
+
+
+#     if method == "GET":
+#         if pk is not None:
+#             # detail view
+#             obj = get_object_or_404(Product, pk=pk)
+#             data = ProductSerializer(obj, many=False).data
+#             return Response(data)
+#         # list view
+#         pass 
+#         queryset = Product.objects.all()
+#         data = ProductSerializer(queryset, many=True).data
+#         return Response(data)
+
+
+#     if method == "POST":
+#         # create an item
+#         serializer = ProductSerializer(data=request.data)    
+#         if serializer.is_valid(raise_exception=True):
+#                 title = serializer.validated_data.get('title')
+#                 content = serializer.validated_data.get('content') or None
+#                 if content is None:
+#                     content = title 
+#                 serializer.save(content=content)
+#                 return Response(serializer.data)
+#         return Response({"Invalid": "not good data"}, status=400),
